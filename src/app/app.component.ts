091a2dcf3105a2d2836cf5dfd654dc59ee92e854
Nazar Kosiv <nazarkosiv94@gmail.com>
2018-03-19 14:28:25 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
+class Todo {
+  title: string;
+  completed: boolean;
+
+  constructor(title: string, completed: boolean = false) {
+    this.title = title;
+    this.completed = completed;
+  }
+}
+
+const todos: Todo[] = [
+  {
+    title: 'some task 1',
+    completed: false
+  },
+  {
+    title: 'some task 2',
+    completed: true
+  },
+  {
+    title: 'some task 3',
+    completed: false
+  }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  todos = todos;
+  newTask: string = '';
+
+  toggle(todo: any) {
+    todo.completed = !todo.completed;
+  }
+
+  delete(todo) {
+    const index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+  }
+
+  add() {
+    this.todos.push(new Todo(this.newTask));
+    this.newTask = '';
+  }
 }
